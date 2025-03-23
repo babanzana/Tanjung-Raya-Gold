c85467813211041b314111fc0ea52c912b80cbbd
Babanzana <babanzana610@gmail.com>
2025-03-24 02:30:49 +0700

    add login

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import { Provider as PaperProvider, Button } from 'react-native-paper';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { Provider as PaperProvider } from "react-native-paper";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginContainer } from "./src/features/login/login.container";
+
+const Stack = createStackNavigator();
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   useEffect(() => {
     const prepare = async () => {
-      await new Promise(resolve => setTimeout(resolve, 2000))
+      await new Promise((resolve) => setTimeout(resolve, 2000));
       SplashScreen.hideAsync();
     };
 
@@ -18,25 +24,13 @@ export default function App() {
 
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        
-        {/* Contoh Button dari React Native Paper */}
-        <Button mode="contained" onPress={() => console.log('Pressed!')}>
-          <Text>Press Me</Text>
-        </Button>
-        
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginContainer} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <StatusBar style="auto" />
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

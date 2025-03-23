2222b05785214c45cdc60c31d26942c0bded77cd
Babanzana <babanzana610@gmail.com>
2025-03-24 02:13:01 +0700

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { Provider as PaperProvider, Button } from 'react-native-paper';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  useEffect(() => {
+    const prepare = async () => {
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      SplashScreen.hideAsync();
+    };
+
+    prepare();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        
+        {/* Contoh Button dari React Native Paper */}
+        <Button mode="contained" onPress={() => console.log('Pressed!')}>
+          <Text>Press Me</Text>
+        </Button>
+        
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
